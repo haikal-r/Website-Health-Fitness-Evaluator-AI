b753493cefef4aa069ad78933843b6e2bc511c6a
Haikal Ramadhan <soonhaikal88@gmail.com>
2024-11-06 19:41:01 +0700

    feat: add new variable font and color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#EF4444",
+                background: "#FEFAF4",
+            },
+        },
+    },
+    plugins: [],
+};
