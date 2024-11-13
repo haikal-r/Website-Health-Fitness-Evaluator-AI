1e1019ec513a89f1c5e11641c7b753423822f52f
adilgunawan <adilgunawan222@gmail.com>
2024-11-13 19:57:09 +0700

    add image and create homepage

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/home', function () {
+Route::get('/', function () {
     return view('home');
+});
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
+Route::get('/questionner1', function () {
+    return view('questionner1');
+});
+Route::get('/questionner2', function () {
+    return view('questionner2');
+});
+Route::get('/questionner3', function () {
+    return view('questionner3');
 });
