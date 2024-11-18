c6f05b6830e80c90cf5cfba871adeba9414807a1
adilgunawan <adilgunawan222@gmail.com>
2024-11-18 15:34:55 +0700

    update dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,36 +3,63 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Dashboard</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
-    <nav class="flex">
-        <div class="mt-7 ml-7 h-screen ">
-            <div>
-                <img src="{{ asset('images/logo.png') }}" class="w-60 h-20" alt="">
-            </div>
-            <div class="px-8 mt-7 items-center space-y-4 ">
-                <ul class="bg-primary px-7 py-3 w-fit h-13 flex rounded-lg items-center text-base gap-4 hover:text-white text-white hover:bg-primary font-bold ">
-                    <ion-icon name="apps-outline" class="text-2xl"></ion-icon>
-                    <li><button>Dashboard</button></li>
-                </ul>
-                <ul class="bg-white px-7 py-3 w-fit h-13 flex rounded-lg items-center text-base gap-4 hover:text-white text-black  hover:bg-primary font-bold">
-                    <ion-icon class="text-3xl " name="person-circle-outline"></ion-icon>
-                    <li><button>Profile</button></li>
-                </ul>
-                <ul class="bg-white px-7 py-3 w-fit h-13 flex rounded-lg items-center text-base gap-4 hover:text-white text-black  hover:bg-primary">
-                    <ion-icon name="apps-outline" class="text-2xl"></ion-icon>
-                    <li><button>Dashboard</button></li>
-                </ul>
-                <ul class="bg-white px-7 py-3 w-fit h-13 flex rounded-lg items-center text-base gap-4 hover:text-white text-black  hover:bg-primary">
-                    <ion-icon name="apps-outline" class="text-2xl"></ion-icon>
-                    <li><button>Dashboard</button></li>
-                </ul>
+    <div>
+        <!-- KONTEN -->
+        <div class="w-[81%] h-screen top-32 left-[16.8rem] absolute bg-background">
+            <div class="bg-white  m-14  h-[770px] rounded-2xl shadow-xl">
+                <h1 class="font-bold text-[32px] ml-9 ">PROGRESS REPORT</h1>
+                <div class=" flex justify-between">
+                    <div class="bg-white shadow-xl m-14 w-[600px] h-64 rounded-2xl p-6">
+                        <h1 class="font-bold text-lg">BMI</h1>
+                        <div class="shadow bg-red-300 m-auto w-7 h-4 p-16 mb-8">CHART TINGGI</div>
+                        <hr>
+                        <div>
+                            <h1 class="text-lg">Height</h1>
+                            <div>
+                                <p>170</p>
+                                <p>CM</p>
+                                <box-icon type='solid' name='pencil'></box-icon>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="bg-white shadow-xl m-14 w-[600px] h-64 rounded-2xl">
+
+                    </div>
+                </div>
+                <div class=" flex justify-between">
+                    <div class="bg-white shadow-xl m-14 w-[600px] h-64 rounded-2xl">
+                
+                    </div>
+                    <div class="bg-white shadow-xl m-14 w-[600px] h-64 rounded-2xl">
+
+                    </div>
+                </div>
             </div>
         </div>
-        <div class="border"></div>
-    </nav>
-    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+        <!-- END CONTENT -->
+
+        <!-- HEADER -->
+        @include('layouts/header')
+        <!-- END HEADER -->
+
+        <!-- SIDE BAR -->
+        @include('layouts/sidebar')
+        <!-- END SIDE BAR -->
+
+        
+    </div>
+   
+
+  
+
+ 
+
+
+
 </body>
 </html>
