36eb063965c5bd8a645fc888cea4f1d78082fcaf
Haikal Ramadhan <soonhaikal88@gmail.com>
2024-11-18 14:44:03 +0700

    feat(login): add feature login

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,12 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 Route::get('/login', [LoginController::class, 'index'])->name('login');
-Route::get('/register', [RegisterController::class, 'index'])->name('register');
+Route::post('/authentication', [LoginController::class, 'authentication'])->name('authentication');
+
+Route::get('/register', [RegisterController::class, 'index'])->name('register.index');
+Route::post('/register-store', [RegisterController::class, 'store'])->name('register.store');
+
