c9254c1b9195c23974f038364440b7e983359dae
Haikal Ramadhan <soonhaikal88@gmail.com>
2024-11-19 14:52:56 +0700

    feat: add profile page, workout references page and dashboard layout

---
+++ resources/views/layouts/dashboard.blade.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="robots" content="noindex, nofollow">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @stack('styles')
+    @stack('scripts')
+
+    <title>@section('title') @show @hasSection('title') - @endif {{ config('app.name') }}</title>
+
+</head>
+
+
+
+<body class="bg-background ">
+    <nav class="relative lg:flex hidden">
+        <div class="fixed top-0 left-0 min-h-screen bg-white z-50 border-e-[1.5px]">
+            <div class="pt-5 px-8">
+                <img src="{{ asset('images/logo.png') }}" class="w-60 h-20" alt="">
+            </div>
+            <div class="px-8 mt-7 items-center space-y-4 ">
+                <ul class="bg-primary px-7 py-3 w-full h-13 flex rounded-lg items-center text-base gap-4 hover:text-white text-white hover:bg-primary font-bold ">
+                    <ion-icon name="apps-outline" class="text-2xl"></ion-icon>
+                    <li><button>Dashboard</button></li>
+                </ul>
+                <ul class="bg-white px-7 py-3 w-full h-13 flex rounded-lg items-center text-base gap-4 hover:text-white text-black  hover:bg-primary font-bold">
+                    <ion-icon class="text-2xl " name="person-circle-outline"></ion-icon>
+                    <li><button>Profile</button></li>
+                </ul>
+                <ul class="bg-white px-7 py-3 w-full h-13 flex rounded-lg items-center text-base gap-4 hover:text-white text-black  hover:bg-primary">
+                    <ion-icon name="pizza" class="text-2xl"></ion-icon>
+                    <li><button>Food</button></li>
+                </ul>
+                <ul class="bg-white px-7 py-3 w-full h-13 flex rounded-lg items-center text-base gap-4 hover:text-white text-black  hover:bg-primary">
+                    <ion-icon name="fitness" class="text-2xl"></ion-icon>
+                    <li><button>Workout</button></li>
+                </ul>
+            </div>
+        </div>
+        <div class="fixed top-0 w-full  bg-white border-b-[1.5px]">
+            <div class="px-10 py-3 flex justify-end">
+                <!-- <img src="{{ asset('images/picture1.jpg') }}" alt="profile" class="rounded-full size-[45px]"> -->
+                <img src="https://image.winudf.com/v2/image/Y29tLnN5YWtpcmEuYW5pbWV3YWxscGFwZXJfc2NyZWVuXzBfMTUyNzA2MTI3MF8wMTg/screen-0.jpg?fakeurl=1&type=.jpg" alt="profile" class="rounded-full size-[35px]">
+            </div>
+        </div>
+    </nav>
+
+
+    <div class="lg:ms-[310px]  flex flex-col justify-center mt-[65px]">
+        @yield('content')
+    </div>
+
+
+
+
+
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+</body>
+
+</html>
